48f64d1ed36f7e0523d066c6a428539df3afd334
P_Shreyas <ssp1996@gmail.com>
2017-11-29 21:09:26 +0530

    Data Model created

---
+++ src/Store.java
@@ -0,0 +1,13 @@
+public class Store {
+    private double value;
+    private String label;
+
+
+    public double getValue() {
+        return value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
